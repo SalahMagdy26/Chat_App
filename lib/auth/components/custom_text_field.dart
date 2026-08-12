3da8886220d04dc129afbc5e01d569bcb4426d44
DESKTOP-V770RIF\SALAHMAGDY <salahmagdy2588@gmail.com>
2026-08-12 15:16:25 +0300

    login ui added

---
+++ lib/auth/components/custom_text_field.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key, required this.hint,
+  });
+  final String hint ;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+
+      decoration: InputDecoration(
+        fillColor: Colors.white,
+        filled: true,
+        border: OutlineInputBorder(borderSide: BorderSide.none,borderRadius: BorderRadius.circular(12)),
+        hint:Text(
+          hint,
+          style: TextStyle(color: Colors.grey.withAlpha(100), fontSize: 15),
+        ),
+      ),
+    );
+  }
+}
--- lib/auth/ui_screens/login_screen.dart
+++ lib/auth/ui_screens/login_screen.dart
@@ -1,19 +1,59 @@
+import 'package:chat_app/auth/components/custom_text_field.dart';
 import 'package:chat_app/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.primaryColor,
-      body: Column(
-        children: [
-            SvgPicture.asset('assets/images/message-solid-full.svg'),
-        ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+
+        backgroundColor: AppColors.primaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/images/message-solid-full.svg',
+                color: Colors.white,
+                width: 150,
+              ),
+              Gap(30),
+              Text(
+                'Welcome to chat app',
+                style: TextStyle(color: Colors.grey.withAlpha(85), fontSize: 15),
+              ),
+              Gap(50),
+              CustomTextField(hint: 'Email',),
+              Gap(20),
+              CustomTextField(hint: 'Password',),
+              Gap(40),
+              GestureDetector(child: Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.orangeAccent.withAlpha(190),
+                  borderRadius: BorderRadius.circular(12),
+
+                ),
+                child: Center(child: Text('Login',style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'saira'
+                ),)),
+              ),)
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
